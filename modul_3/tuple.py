12eba70134fc4ec033e059e882afbf203fd54ddc
Yasemin Şireci <yaseminsireci@gmail.com>
2025-12-14 02:11:04 +0300

    Add files via upload

---
+++ modul_3/tuple.py
@@ -0,0 +1,15 @@
+"""
+Tuple 
+*Değiştirilemezler
+*Elemanlarının index-sıra numarası vardır
+"""
+my_tuple = (1,2,3,"python")
+my_list = [1,2,3,3,5,5]
+tup = set(my_list)
+print(tup)
+# print(my_tuple[0])
+
+# my_tuple[0] = 100
+
+# for i in my_tuple:
+#     print(i)
